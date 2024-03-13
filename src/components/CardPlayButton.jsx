cb9e92bb9125677e1294600b74d892655db0c3c0
AylenRondan <rrondancarrizo@gmail.com>
2024-03-13 09:50:06 -0300

    spotify-clone

---
+++ src/components/CardPlayButton.jsx
@@ -0,0 +1,38 @@
+import { Pause, Play } from "./Player"
+import { usePlayerStore } from '../store/playerStore'
+
+export function CardPlayButton ({ id, size = 'small' }) {
+  const {
+    currentMusic,
+    isPlaying,
+    setIsPlaying,
+    setCurrentMusic
+  } = usePlayerStore(state => state)
+
+  const isPlayingPlaylist = isPlaying && currentMusic?.playlist.id === id
+
+  const handleClick = () => {
+    if (isPlayingPlaylist) {
+      setIsPlaying(false)
+      return
+    }
+
+    fetch(`/api/get-info-playlist.json?id=${id}`)
+      .then(res => res.json())
+      .then(data => {
+        const { songs, playlist } = data
+
+        setIsPlaying(true)
+        setCurrentMusic({ songs, playlist, song: songs[0] })
+      })
+  }
+
+  const iconClassName = size === 'small' ? 'w-4 h-4' : 'w-5 h-5'
+
+
+  return (
+    <button onClick={handleClick} className="relative card-play-button absolute rounded-full bg-green-500 p-4 left-28 hover:scale-110 transition ">
+      {isPlayingPlaylist ? <Pause className={iconClassName} /> : <Play className={iconClassName} />}
+    </button>
+  )
+}
---
+++ src/pages/api/get-info-playlist.json.js
@@ -0,0 +1,16 @@
+import { allPlaylists, songs as allSongs} from "../../lib/data";
+
+export async function GET({params, request }) {
+    //get id from url
+    const {url}=request
+    const urlObject = new URL(url)
+    const id = urlObject.searchParams.get('id')
+    //get the playlist info
+    const playlist = allPlaylists.find((playlist) => playlist.id === id)
+    const songs = allSongs.filter(song => song.albumId === playlist?.albumId)
+
+    return new Response(JSON.stringify({playlist,songs}),{
+        headers: {"content-type" : "application/json"}
+    })
+
+}
